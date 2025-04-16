b583b9e934590c6c008ba266db94f5f5321610de
sravanipopuri2006 <popurisirisha75@gmail.com>
2025-04-16 08:44:24 +0530

    commit

--- NumberToWords.java
+++ NumberToWords.java
@@ -4,21 +4,34 @@ import java.util.*;
 public class NumberToWords {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the NUmber: ");
         int num=sc.nextInt();
         String units[]={"","one","two","three","four","five","six","seven","eight","nine"};
         String Teens[]={"","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+        String teen[]={"","Eleven","tewlve","thirteen","fourteen","fifteen","Sixteen","Seventeen","Eighteen","Ninteen"};
         if(num==10){
             System.out.println("Ten");
         }
         if(num==100){
             System.out.println("Hundred");
         }
+        if(num==0){
+            System.out.println("Zero");
+        }
+        if(num>10 && num<20){
+            System.out.println(teen[num%10]);
+        }
+        if(num>1000){
+            System.out.print(units[num/1000]+" Thousand");
+            num=num%1000;
+        }
         if(num>100){
             System.out.print(units[num/100]+" Hundered and ");
             num=num%100;
 
         }
-        if(num>=10){
+        
+        if(num>=20){
             System.out.print(Teens[num/10]+" ");
             num=num%10;
 
